9266bae884a53c61521129c13f9389f0b70beb35
alexvuce <104812878+alexvuce@users.noreply.github.com>
2025-12-01 12:23:49 -0500

    Update current_ratio.sql

--- teaching/dab-304/lab/current-ratio/current_ratio.sql
+++ teaching/dab-304/lab/current-ratio/current_ratio.sql
@@ -1,1 +1,11 @@
+-- Purpose of code:
 
+with fact as (
+  select fiscal_periodi_id
+        ,organization_sector_id
+        ,pa_id
+        ,sa_id
+        ,amt
+  from ohfs_r.fact_fin
+)
+select top 1000 * from fact
